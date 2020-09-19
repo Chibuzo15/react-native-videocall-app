7f59e36dfdf3be2ba4d8950be3b59d345aa8d181
Chibuzo15 <Anyachris85@gmail.com>
2020-09-19 08:01:22 +0100

    Reversed expo eject, migrated to react native init instead

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -26,7 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
